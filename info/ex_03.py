103e511c486bb8bf80e72a8f49a75c7fd96e3d9f
heojungyoung <tyireal@gmail.com>
2025-07-01 19:08:39 +0900

    03.py

---
+++ info/ex_03.py
@@ -0,0 +1,13 @@
+str = 'Sinagong'
+n = len(str)
+st = list()
+for k in range(n):
+    st.append(str[k])
+for k in range(n-1, -1, -1):
+    print(st[k], end = '')    
+
+# gnoganiS        
+    
+# st = ['S','i','n','a','g','o','n','g'].    
+
+# iterates k from 7 down to 0, printing each character with no newline or spacing.
