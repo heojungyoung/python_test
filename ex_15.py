56547ee98c963c15dd98daa65011e47a4aa4f63d
heojungyoung <tyireal@gmail.com>
2025-07-02 22:41:49 +0900

    15

---
+++ ex_15.py
@@ -0,0 +1,22 @@
+x = 'xyz321'
+y = 'opq654'
+
+a = ['abc123', 'def456', 'ghi789' ]
+a.append(x)
+a.append(y)
+a.remove('def456')
+print(a[1][-3:], a[2][:-3], sep=',')
+for i in range(3,6):
+    print(i, end = ' ')
+ 
+# 789,xyz
+# 3 4 5
+
+# ['abc123', 'ghi789', 'xyz321', 'opq654']
+#   0         1         2         3
+
+# a[1][-3:]
+# 'ghi789' 의 마지막 3글자 → '789'
+
+# a[2][:-3]
+# 'xyz321' 의 뒤 3글자를 뺀 앞부분 → 'xyz'
