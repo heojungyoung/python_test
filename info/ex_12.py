d1eb12dd02ef3ef3481286ec3804a1518b25ad5f
heojungyoung <tyireal@gmail.com>
2025-07-02 22:12:04 +0900

    10, 12

---
+++ info/ex_12.py
@@ -0,0 +1,11 @@
+a = 100
+result = 0
+for i in range(1,3):
+    result = a >> i
+    result = result + 1
+print(result)    
+
+
+# i	a >> i (비트 우측 이동)	+ 1	result 값
+# 1	100 >> 1 → 50	51	51
+# 2	100 >> 2 → 25	26	26
