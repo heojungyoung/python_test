48a03f6299cf6e4eb71bc2cc43d272305283de42
heojungyoung <tyireal@gmail.com>
2025-07-02 16:12:38 +0900

    08 commit

---
+++ info/ex_08.py
@@ -0,0 +1,20 @@
+a = ['Seoul' , 'Kyeonggi', 'Incheon', 'Daejeon', 'Daegu', 'Pusan' ];
+
+str01 = 'S'
+
+for i in a:
+    str01 = str01 + i[1]
+
+print(str01)    
+
+
+# Seynaau
+
+# 각 키워드 두번째 글자
+
+# 'Seoul' → 'e'  
+# 'Kyeonggi' → 'y'
+# 'Incheon' → 'n'
+# 'Daejeon' → 'a'
+# 'Daegu' → 'a'
+# 'Pusan' → 'u'
